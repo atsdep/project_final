df51660e96f477631353dbfd414b1560a45000da
atsdep <adthasid@hotmail.com>
2016-10-08 17:40:57 +0700

    new update 1

---
+++ new.php
@@ -0,0 +1,302 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>Start</title>
+		<meta name="description" content="">
+		<meta name="author" content="Adthasid">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<!-- css -->
+		<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/studyOne.css">
+		<link rel="stylesheet" href="css/studyTwo.css">
+
+		<link rel="stylesheet" href="css/new.css">
+
+		<link rel="stylesheet" href="css/adthasid.css">
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	</head>
+
+	<body class="with-new-header ">
+
+		<div id="header" class="airbnb-header new hide-sm logged_in is_host">
+
+			<header class="regular-header clearfix hide-sm" id="new-header" role="banner">
+
+				<div class="comp pull-left">
+					<a href="/" class="hdr-btn link-reset belo-container text-logo" aria-label="Rentcnd"> Rentcnd </a>
+				</div>
+
+			</header>
+
+		</div>
+
+		<script src="https://a0.muscache.com/airbnb/static/packages/header_cookie.bundle-cbed4f8007c1e3c328f8.js" type="text/javascript"></script>
+
+		<main id="site-content" role="main">
+
+			<meta name="pinterest" content="nopin">
+
+			<script src="https://a0.muscache.com/airbnb/static/vendor/flexibility-27006ad9f79eed4c38dff451e54b9b9a.js" type="text/javascript"></script>
+
+			<div id="lys-redesign-div">
+				<div>
+					<div>
+						<div class="bg-white">
+							<div>
+								<div class="airbnb-header new list-your-space__header list-your-space__header-sm bg-medium show-sm">
+									<a href="/" class="link-reset belo-container pull-left text-logo" aria-label="Rentcnd"> Rentcnd </a>
+								</div>
+								<div class="h5 text-normal lys-navbar__preview-link-container show-sm">
+									<div class="lys-navbar__preview-link preview-link-not-available">
+										<a class="" href="/rooms/undefined?preview_for_lys" target="_blank"><span>ตัวอย่าง</span></a>
+									</div>
+								</div>
+							</div>
+							<div class="landing-width bg-white row row-condensed list-your-space__landing-content">
+								<span>
+									<div class="col-md-7 landing__left-col fast-animation">
+										<!-- readyToPublish ตอนเสร็จหมดแล้ว -->
+										<div class="landing__left-col-content">
+											<h2 class="landing__title-title space-5"><span>กลายเป็นเจ้าของรถเช่า Rentcnd</span></h2>
+
+											<div class="landing-step-collection step-collection-completed">
+												<div class="row row-condensed lys-vertical-align-middle-container">
+													<div class="col-sm-10">
+														<div class="va-container va-container-v va-container-h">
+															<div class="va-middle">
+																<div class="landing__step-content">
+																	<strong class="landing__step-number text-base text-branding text-light-gray"><span>ขั้นตอน 1</span></strong>
+																	<div class="h3 landing__step-content-title landing__subtitle-width">
+																		เริ่มที่สิ่งของพื้นฐาน
+																	</div>
+																	<div class="landing__step-content-subtitle">
+																		เตียง ห้องน้ำ สิ่งอำนวยความสะดวกและอื่นๆ
+																	</div><span><span class="" style="transition-delay:0ms;display:inline-block;"><a class="link-babu" href="/become-a-host/15415428/room"><span>เปลี่ยน</span></a></span></span>
+																</div>
+															</div>
+														</div>
+													</div>
+													<div class="col-sm-2 correct">
+														<span>
+															<div class="text-center" style="transition-delay:0ms;transition-duration:250ms;">
+																<i class="icon icon-ok-fill icon-size-2 icon-babu"></i>
+															</div></span>
+													</div>
+												</div>
+												<hr>
+											</div>
+											<!-- step-1-com -->
+
+											<div data-reactid="step-1">
+												<div class="landing-step-collection">
+													<div class="row row-condensed lys-vertical-align-middle-container">
+														<div class="col-sm-10">
+															<div class="va-container va-container-v va-container-h">
+																<div class="va-middle">
+																	<div class="landing__step-content">
+																		<strong class="landing__step-number text-base text-branding text-light-gray"><span>ขั้นตอน 1</span></strong>
+																		<div class="h3 landing__step-content-title landing__subtitle-width">
+																			เริ่มที่ข้อมูลพื้นฐาน
+																		</div>
+																		<div class="landing__step-content-subtitle">
+																			ประเภทรถ สิ่งอำนวยความสะดวกและอื่นๆ
+																		</div>
+																		<div class="progress landing__progress space-top-2" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+																			<div class="progress-bar progress-bar-babu landing__progress-bar" style="width:50%;"></div>
+																		</div><span><span class="">
+																				<div>
+																					<a class="" href="/become-a-host/location">
+																					<button class="btn btn-babu space-top-1 text-large">
+																						<span>ดำเนินการต่อ</span>
+																					</button></a>
+																				</div></span></span>
+																	</div>
+																</div>
+															</div>
+														</div>
+														<div class="col-sm-2 lys-vertical-align-middle">
+
+														</div>
+													</div>
+												</div>
+												<hr>
+											</div>
+											<!-- step1 -->
+
+											<div data-reactid="step-2">
+												<div class="landing-step-collection">
+													<div class="row row-condensed lys-vertical-align-middle-container">
+														<div class="col-sm-10">
+															<div class="va-container va-container-v va-container-h">
+																<div class="va-middle">
+																	<div class="landing__step-content">
+																		<strong class="landing__step-number text-base text-branding text-light-gray"><span>ขั้นตอน 2</span></strong>
+																		<div class="h3 landing__step-content-title landing__subtitle-width">
+																			รูปภาพ
+																		</div>
+																		<div class="landing__step-content-subtitle">
+																			รูป รายละเอียดสั้นๆ ชื่อประกาศ
+																		</div>
+																		<div class="progress landing__progress space-top-2" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+																			<div class="progress-bar progress-bar-babu landing__progress-bar" style="width:50%;"></div>
+																		</div><span><span class="">
+																				<div>
+																					<a class="" href="/become-a-host/location">
+																					<button class="btn btn-babu space-top-1 text-large">
+																						<span>ดำเนินการต่อ</span>
+																					</button></a>
+																				</div></span></span>
+																	</div>
+																</div>
+															</div>
+														</div>
+														<div class="col-sm-2 lys-vertical-align-middle">
+
+														</div>
+													</div>
+												</div>
+												<hr>
+											</div>
+											<!-- step2 -->
+
+											<div data-reactid="step-3">
+												<div class="landing-step-collection">
+													<div class="row row-condensed lys-vertical-align-middle-container">
+														<div class="col-sm-10">
+															<div class="va-container va-container-v va-container-h">
+																<div class="va-middle">
+																	<div class="landing__step-content">
+																		<strong class="landing__step-number text-base text-branding text-light-gray"><span>ขั้นตอน 3</span></strong>
+																		<div class="h3 landing__step-content-title landing__subtitle-width">
+																			เตรียมพร้อมสำหรับผู้ใช้บริการ
+																		</div>
+																		<div class="landing__step-content-subtitle">
+																			ราคา ปฎิทิน การตั้งค่าการจอง
+																		</div>
+																		<div class="progress landing__progress space-top-2" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+																			<div class="progress-bar progress-bar-babu landing__progress-bar" style="width:50%;"></div>
+																		</div><span><span class="">
+																				<div>
+																					<a class="" href="/become-a-host/location">
+																					<button class="btn btn-babu space-top-1 text-large">
+																						<span>ดำเนินการต่อ</span>
+																					</button></a>
+																				</div></span></span>
+																	</div>
+																</div>
+															</div>
+														</div>
+														<div class="col-sm-2 lys-vertical-align-middle">
+
+														</div>
+													</div>
+												</div>
+												<hr>
+											</div>
+											<!-- step3 -->
+
+											<span>
+												<div class="landing__publish-section">
+													<h3></h><span>เท่านี้เอง! คุณพร้อมเผยแพร่แล้ว</span></h3>
+													<p class="text-gray">
+														<span>ไม่นานหลังจากนี้ รถเช่าของคุณจะปรากฏบน Rentcnd ผู้ใช้คนอื่นสามารถจองตามที่ปฏิทินของคุณแสดงว่าบริการรถเช่าพร้อมคนขับของคุณว่าง</span>
+													</p>
+													<p class="text-rausch">
+														<span>*** คุณสามารถยืนยันรถเช่า และ ตัวตนของคุณเพื่อความน่าเชื่อถือและความไว้ใจแก่ผู้ใช้บริการคนอื่นๆ</span>
+													</p>
+													<div class="va-container va-container-v">
+														<button type="button" class="btn btn-primary va-middle text-large">
+															<span>ลงประกาศรถเช่า</span>
+														</button>
+													</div>
+												</div></span>
+
+										</div>
+										<!-- landing__left-col-content -->
+
+									</div> <!-- col-md-7 -->
+									<div class="col-md-5 landing__right-col">
+										<div>
+											<div class="landing__animation hide-sm lys-vertical-align-middle-container">
+												<span>
+													<div style="transition-duration:250ms;">
+														<div class="landing__svg-container lys-vertical-align-middle">
+															<img src="img/flower.png" />
+														</div>
+													</div></span>
+											</div>
+
+											<!-- <div class="landing__more-info-card">
+												<div class="hide-sm">
+													<span>
+														<div class="panel wmpw-panel fast-animation">
+
+															<div class="panel-body">
+																<div class="help-panel__bulb-img space-2"></div>
+																<div class="text-large text-gray">
+																	<span>กรอกข้อมูลให้ครบเพื่อความสะดวกแก่ผู้ใช้บริการ</span>
+																</div>
+															</div>
+														</div> </span>
+												</div>
+											</div> -->
+
+											<div class="landing__more-info-card">
+												<div class="preview-card text-center text-large">
+													<span>
+														<div class="preview-card--border media space-top-2 hide-sm">
+															<a href="/rooms/15415428?preview_for_lys" target="_blank" class="link-reset link--accessibility-outline" alt="ตัวอย่างประกาศ">
+															<div class="panel-body va-container">
+																<div class="media-body preview-card--right-col va-middle text-truncated">
+																	<span>ชื่อประกาศ</span>
+																	<br>
+																	<span class="text-rausch"><span>ตัวอย่าง</span></span>
+																</div>
+																<div class="pull-right media-photo preview-card__img-container" style="height:69px;width:104px;background-position:center;background-size:cover;">
+																	<div title="ภาพหน้าแรกของรถเช่า" style="height:69px;width:104px;background-position:center;background-size:cover;background-image:url(../img/car.jpg?aki_policy=x_small);"></div>
+																</div>
+															</div></a>
+														</div></span>
+												</div>
+											</div>
+
+										</div>
+									</div> <!-- col-md-7 --> </span><!-- span main -->
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-2 lys-vertical-align-middle">
+
+				</div>
+			</div>
+			<!-- lys-redesign-div -->
+
+		</main>
+
+		<?php
+		include 'include/footer.php';
+		?>
+	</body>
+	<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+	<script src="js/jquery-3.1.0.min.js" type="text/javascript"></script>
+
+</html>
