96766e9b8f5ecad08b85c4942aa9987aa79a71ea
atsdep <adthasid@hotmail.com>
2016-10-09 14:29:54 +0700

    update

---
+++ include/all_header.php
@@ -0,0 +1,157 @@
+<div id="header" class="airbnb-header new  logged_in">
+	<div class="header--sm show-sm" aria-hidden="true">
+		<a href="#" aria-label="หน้าแรก" data-prevent-default="" class="link-reset burger--sm"> <i class="icon icon-reorder icon-rausch"></i> <span class="screen-reader-only"> Airbnb </span> </a>
+		<div class="title--sm text-center">
+			<button class="btn btn-block search-btn--sm search-modal-trigger hide" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+				<i class="icon icon-search icon-gray"></i>
+				<span class="search-placeholder--sm"> ไปที่ไหน </span>
+			</button>
+			<a href="/" class="header-belo hide"> <span class="screen-reader-only"> Rentcnd </span> </a>
+			<a href="/" class="hdr-btn link-reset belo-container text-logo" aria-label="Rentcnd"> Rendcnd </a>
+		</div>
+		<div class="action--sm"></div>
+		<nav class="nav--sm" role="navigation"></nav>
+		<div class="search-modal-container"></div>
+	</div>
+
+	<header class="regular-header clearfix hide-sm" id="new-header" role="banner">
+
+		<div class="comp pull-left">
+			<a href="/" class="hdr-btn link-reset belo-container text-logo" aria-label="Rentcnd"> Rendcnd </a>
+		</div>
+
+		<div class="comp pull-right show-login">
+			<a class="no-crawl hdr-btn link-reset header-avatar-trigger needsclick" rel="nofollow" href="/dashboard" data-href="/dashboard" data-href-host="/trips"> <span class="value_name margin-right--tiny hide-md">Adthasid</span>
+			<div class="media-photo media-round user-profile-image header-icon icon-profile-alt-gray"><img width="28" height="28" src="img/profile.jpg?aki_policy=profile_small" alt="">
+			</div> </a>
+
+			<div class="panel  drop-down-menu avatar-tooltip--new">
+				<div class="panel-header no-border hide-lg">
+					<strong><span class="value_name">Adthasid</span></strong>
+				</div>
+				<a href="/users/edit" class=" panel-body link-reset hover-item no-crawl">
+				<div class="hover-item__content">
+					การเดินทางของคุณ
+				</div> </a>
+				<a href="/users/edit" class=" panel-body link-reset hover-item no-crawl">
+				<div class="hover-item__content">
+					แก้ไขข้อมูลส่วนตัว
+				</div> </a>
+				<a href="/users/notifications" class=" panel-body link-reset hover-item no-crawl">
+				<div class="hover-item__content">
+					การตั้งค่าบัญชี
+				</div> </a>
+				<a href="/logout" class=" panel-body link-reset hover-item no-crawl">
+				<div class="hover-item__content">
+					ออกจากระบบ
+				</div> </a>
+			</div>
+		</div>
+
+		<div class="comp pull-right">
+			<a data-login-modal="" href="/login" data-redirect="redirect_params[action]=host&amp;redirect_params[controller]=info" class="hdr-btn link-reset"> เข้าสู่ระบบ </a>
+		</div>
+		<div class="comp pull-right">
+			<a data-signup-modal="" data-header-view="true" href="/signup_login" data-redirect="redirect_params[action]=host&amp;redirect_params[controller]=info" class="hdr-btn link-reset"> ลงทะเบียน </a>
+		</div>
+
+		<div class="comp pull-right show-travel-credit js-travel-credit-menu-container hide">
+			<a class="needsclick no-crawl hdr-btn link-reset js-travel-credit-comp" href="/invite?r=3" rel="nofollow" data-href="/invite?r=3">
+			<div class="gift-icon-container text-center">
+				<span class="margin-right--tiny hide-md">เครดิต</span>
+				<i class="header-icon js-travel-credit-icon icon-gift-alt-gray"> <i class="alert-count js-travel-credit-unread-count text-center fade in">1</i> </i>
+			</div> </a>
+
+			<div class="panel  drop-down-menu travel-credit-dropdown"></div>
+		</div>
+
+		<div class="comp pull-right">
+			<a class="needsclick host-icon-wrapper no-crawl hdr-btn link-reset js-host-menu-icon" href="/dashboard" rel="nofollow" data-href="/dashboard"> <span class="hide-md margin-right--tiny"> เจ้าของรถเช่า </span> <i class="fa fa-car js-host-icon carhead"> <i class="alert-count js-host-item-count listing-count text-center fade">0</i> </i> </a>
+
+			<div class="panel  drop-down-menu host-tooltip--new">
+				<div class="panel-header no-border hide-lg">
+					<span><strong>เจ้าของรถเช่า</strong></span>
+				</div>
+				<div class="hover-item panel-body relist-your-space hide">
+					<a href="#" class="link-reset click-area">
+					<div class="cta-container va-container va-container-v">
+						<div class="va-middle">
+							<div class="cta-text">
+								ลงประกาศรถเช่าให้เสร็จ
+							</div>
+							<div class="cta-text-v3-reactivate hide">
+								เปิดใช้งานบัญชีใหม่
+							</div>
+							<div class="progress">
+								<div class="progress-bar"></div>
+							</div>
+						</div>
+					</div> </a>
+					<div class="close-button hide">
+						<a href="#" class="alert-close"></a>
+					</div>
+				</div>
+
+				<a href="/dashboard" class=" panel-body link-reset hover-item origin-item js-host-dashboard-link no-crawl">
+				<div class="hover-item__content">
+					แดชบอร์ด
+					<i class="alert-count js-pending-request-count--inline text-center fade">0</i>
+				</div> </a>
+				<a href="/rooms" class=" panel-body link-reset hover-item origin-item js-host-calendar-link hide no-crawl">
+				<div class="hover-item__content">
+					ปฏิทิน
+
+				</div> </a>
+				<a href="/rooms" class=" panel-body link-reset hover-item origin-item js-host-manage-listings-link no-crawl">
+				<div class="hover-item__content">
+					จัดการรถเช่า
+					<i class="alert-count listing-count listing-count--inline text-center fade">0</i>
+				</div> </a>
+				<a href="/rooms/new" class=" panel-body link-reset hover-item origin-item js-host-list-your-space-link">
+				<div class="hover-item__content">
+					ลงประกาศรถเช่า
+
+				</div> </a><a href="/my_reservations" class=" panel-body link-reset hover-item origin-item js-host-my-reservations-link no-crawl">
+				<div class="hover-item__content">
+					การจองของลูกค้า
+
+				</div> </a>
+				<a href="/users/transaction_history" class=" panel-body link-reset hover-item origin-item js-host-transaction-history-link no-crawl">
+				<div class="hover-item__content">
+					การจองของคุณ
+
+				</div> </a>
+				<a href="/users/reviews" class=" panel-body link-reset hover-item origin-item js-host-reviews-link no-crawl">
+				<div class="hover-item__content">
+					ความคิดเห็น
+
+				</div> </a>
+				<a href="/host-assist" class=" panel-body link-reset hover-item origin-item hide item-host-assist js-host-assist-link no-crawl">
+				<div class="hover-item__content">
+					เจ้าของรถเช่าช่วย
+
+				</div> </a>
+			</div>
+		</div>
+
+		<div class="comp pull-right hide-host comp-become-a-host">
+			<a class="hdr-btn link-reset lys-link js-become-a-host-link comp-become-a-host__link" href="new.php"> <span class="btn btn-small btn-become-a-host"> สร้างรายได้จากการขับรถ</span> </a>
+		</div>
+
+		<div class="comp pull-left back-to-search-results-wrapper"></div>
+
+		<div class="comp search-bar-wrapper" role="search">
+			<form action="/s" class="search-form">
+				<div>
+					<div class="search-bar">
+						<i class="header-icon icon-search-alt-gray search-icon" aria-hidden="true"></i><label class="screen-reader-only" for="header-search-form" aria-hidden="true">ไปที่ไหน</label>
+						<input type="text" placeholder="ไปที่ไหน" autocomplete="off" name="location" id="header-search-form" class="location" value="">
+						<input type="hidden" name="source" value="hdr">
+					</div><div id="header-search-settings" class="panel search-settings header-menu"></div>
+				</div>
+			</form>
+		</div>
+
+	</header>
+
+</div>
