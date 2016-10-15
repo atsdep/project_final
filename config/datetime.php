265e436c7a3e283524afac2bfbb24c724774b05d
atsdep <adthasid@hotmail.com>
2016-10-15 16:26:32 +0700

    fix bug function DateThai

--- config/datetime.php
+++ config/datetime.php
@@ -7,15 +7,4 @@ $thaimonth = array("", "มกราคม", "กุมภาพันธ์", "
 
 //echo $thaiweek[date("w")], "ที่", date(" j "), $thaimonth[date("m") - 1], " พ.ศ. ", date(" Y ") + 543;
 
-function DateThai($strDate) {
-	$strYear = date("Y", strtotime($strDate));
-	$strMonth = date("n", strtotime($strDate));
-	$strDay = date("j", strtotime($strDate));
-	$strHour = date("H", strtotime($strDate));
-	$strMinute = date("i", strtotime($strDate));
-	$strSeconds = date("s", strtotime($strDate));
-	$strMonthCut = ARRAY("", "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม");
-	$strMonthThai = $strMonthCut[$strMonth];
-	return "$strDay $strMonthThai $strYear";
-}
 ?>
