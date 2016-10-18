40a6f0b1bf4230e4e6aeac4816e79730f51b894a
atsdep <adthasid@hotmail.com>
2016-10-19 05:05:37 +0700

    no use

---
+++ booking-detail.php
@@ -0,0 +1,282 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>รายละเอียดการจองของคุณ</title>
+		<meta name="description" content="">
+		<meta name="author" content="Adthasid">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<!-- css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/studyOne.css">
+		<link rel="stylesheet" href="css/studyTwo.css">
+
+		<link rel="stylesheet" href="css/studybooking.css">
+		<link rel="stylesheet" href="css/studybookingp4.css">
+
+		<link rel="stylesheet" href="css/cars.css">
+
+		<link rel="stylesheet" href="css/adthasid.css">
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+		<?php
+		include 'config/js.php';
+		?>
+
+	</head>
+
+	<body class="with-new-header">
+
+		<?php
+		include 'include/all_header.php';
+		?>
+
+		<main id="site-content" role="main">
+			<div id="main-view" class="main-view page-container-responsive space-top-md-6 space-md-6 space-top-lg-6 space-lg-6">
+
+				<div class="row">
+					<div class="col-md-5 col-md-push-7 space-lg-2 space-md-2 side-summary-container">
+						<div data-hypernova-key="p4_sidebarbundlejs">
+							<div class="summary-card col-center">
+								<div class="background-cover summary-card__payments-listing-image" style="background-image:url(https://a1.muscache.com/im/pictures/18393650/49be6798_original.jpg?aki_policy=large);"></div>
+								<div class="pull-right space-3 summary-card__host-profile-photo">
+									<div class="media-photo media-round"><img class="summary-card__host-profile-photo-src" src="https://a1.muscache.com/im/users/4971869/profile_pic/1431945187/original.jpg?aki_policy=profile_x_medium" alt="Sabelli House">
+									</div>
+								</div>
+								<div class="panel">
+									<div class="panel-body">
+										<div class="text-muted space-2">
+											<span>ให้เช่าที่พักโดย Sabelli House</span>
+										</div>
+										<div class="sidebar-text-large">
+											CARAVAGGIO
+										</div>
+										<div class="hide-sm text-muted">
+											<ul class="list-layout summary-card__additional-details-list">
+												<li>
+													บ้าน/อพาร์ทเมนท์ทั้งหลัง
+												</li>
+												<li>
+													<small>
+														<div class="star-rating-wrapper" aria-label="p3.summary.number of stars and reviews">
+															<div class="star-rating" content="4">
+																<div class="foreground">
+																	<span><span><i class="icon-star icon icon-beach icon-star-big"></i><span> </span></span><span><i class="icon-star icon icon-beach icon-star-big"></i><span> </span></span><span><i class="icon-star icon icon-beach icon-star-big"></i><span> </span></span><span><i class="icon-star icon icon-beach icon-star-big"></i><span> </span></span></span>
+																</div>
+																<div class="background">
+																	<span><span><i class="icon-star icon icon-light-gray icon-star-big"></i><span> </span></span><span><i class="icon-star icon icon-light-gray icon-star-big"></i><span> </span></span><span><i class="icon-star icon icon-light-gray icon-star-big"></i><span> </span></span><span><i class="icon-star icon icon-light-gray icon-star-big"></i><span> </span></span><span><i class="icon-star icon icon-light-gray icon-star-big"></i><span> </span></span></span>
+																</div>
+															</div><span> </span><span class="h6"><small><span>5</span></small></span>
+														</div></small>
+												</li>
+												<li class="hide-md">
+													<span>ความคิดเห็น 5 ข้อความ</span>
+												</li>
+											</ul>
+											<div>
+												Via dei Sabelli, โรม, Lazio 00185, อิตาลี
+											</div>
+										</div>
+									</div>
+									<div class="panel-body hide-sm">
+										<div class="row row-condensed">
+											<div class="col-sm-5">
+												<div class="text-muted space-bottom-2">
+													<span>เช็คอิน</span>
+												</div><span>29 พฤศจิกา 2016</span>
+											</div>
+											<div class="col-sm-2 summary-card__check-in-icon">
+												<i class="icon icon-chevron-right icon-light-gray"></i>
+											</div>
+											<div class="col-sm-5">
+												<div class="text-muted space-bottom-2">
+													<span>เช็คเอาท์</span>
+												</div><span>16 ธันวา 2016</span>
+											</div>
+										</div>
+									</div>
+									<div class="panel-body hide-sm">
+										<table class="summary-card__billing-table">
+											<tbody>
+												<tr class="price-item">
+													<th class="price-item__header" scope="row"><span>฿1767 x 17 คืน</span><span>&nbsp;</span><span><span><i class="icon icon-question"></i></span></span></th><td class="text-right price-item__price">
+													<div class="">
+														<span>฿30037</span>
+													</div></td>
+												</tr>
+												<tr class="price-item">
+													<th class="price-item__header" scope="row"><span>ค่าทำความสะอาด</span><span>&nbsp;</span><span><span><i class="icon icon-question"></i></span></span></th><td class="text-right price-item__price">
+													<div class="">
+														<span>฿400</span>
+													</div></td>
+												</tr>
+												<tr class="price-item">
+													<th class="price-item__header" scope="row"><span>ค่าบริการ</span><span>&nbsp;</span><span><span><i class="icon icon-question"></i></span></span></th><td class="text-right price-item__price">
+													<div class="">
+														<span>฿3382</span>
+													</div></td>
+												</tr>
+												<tr>
+													<td colspan="2">
+													<div class="text-left">
+														<a data-prevent-default="true" href="#"><span>คูปอง</span></a>
+													</div></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+									<div class="panel-body hide-sm">
+										<button class="sidebar-text-large space-2 summary-card__total-price" tabindex="-1">
+											<table class="summary-card__billing-table">
+												<tbody>
+													<tr class="price-item">
+														<th class="price-item__header" scope="row"><span>ทั้งหมด</span><span>&nbsp;</span></th><td class="text-right price-item__price">
+														<div class="">
+															<span>฿33819</span><sup>THB</sup>
+														</div></td>
+													</tr>
+												</tbody>
+											</table>
+										</button>
+									</div>
+									<div class="show-sm panel-body text-center">
+										<a><span>รายละเอียดการจองของคุณ</span></a>
+									</div>
+									<input type="hidden" value="1" name="apply_airbnb_credit" id="apply_airbnb_credit">
+								</div>
+								<div class="hide-sm">
+									<div class="risk-messagebox-container space-top-md-4 space-top-lg-5">
+										<div class="risk-messagebox-pane row row-condensed">
+											<div class="va-container va-container-h va-container-v">
+												<div class="col-sm-10">
+													<h4 class="risk-messagebox__title space-top-1"><span>Airbnb จะไม่ขอให้คุณโอนเงินเด็ดขาด</span></h4>
+													<hr>
+												</div>
+												<div class="va-middle col-sm-2">
+													<div class="img--air-defender-no-overlap text-right"></div>
+												</div>
+											</div>
+											<div class="col-sm-10 col-md-12 col-lg-12 risk-messagebox__content">
+												<span>การชำระเงินผ่านเว็บไซต์หรือแอพ Airbnb จะช่วยรักษาความปลอดภัยของการชำระเงินของคุณ และช่วยให้คุณได้รับการคุ้มครองภายใต้นโยบายที่บังคับใช้</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="pwp-summary-container"></div>
+
+					</div>
+
+					<div id="content-container" class="summary-card-page-content col-md-7 col-md-pull-5">
+						<div class="accordion-panel" data-panel-name="AboutYourTrip">
+							<div class="accordion-panel__content accordion-panel--expanded">
+								<div data-hypernova-key="p4_first_messagebundlejs">
+									<div class="p4-first-message">
+
+										<div id="details" class="details-section webkit-render-fix">
+											<div class="page-container-responsive">
+												<div class="row">
+													<div class="col-lg-6 js-details-column">
+														<div class="space-8 space-top-8">
+															<h4 class="space-4 text-center-sm"><span><span>รายละเอียดการจองของคุณ </span></span></h4>
+															<hr>
+															<div class="row">
+																<div class="col-md-3 text-muted">
+																	<div>
+																		<span>รถเช่า</span>
+																	</div>
+																	<div class="edit-link--hide">
+																		<i class="icon icon-edit icon-rausch"></i><span>&nbsp;</span><a href="/manage-listing/15417960/basics"><strong> <span>แก้ไข</span> </strong> </a>
+																	</div>
+																</div>
+																<div class="col-md-9">
+																	<div class="row">
+																		<div class="col-md-4">
+																			<div>
+																				<span>ยี่ห้อ:</span><span>&nbsp;</span><strong>Toyota</strong>
+																			</div>
+																			<div>
+																				<span>รุ่น:</span><span>&nbsp;</span><strong>Yaris</strong>
+																			</div>
+																			<div>
+																				<span>ปี:</span><span>&nbsp;</span><strong>2010</strong>
+																			</div>
+																		</div>
+																		<div class="col-md-7">
+																			<div>
+																				<span>ประเภท:</span><span>&nbsp;</span><strong>รถเก๋ง</strong>
+																			</div>
+																			<div>
+																				<span>รองรับผู้โดยสาร:</span><span>&nbsp;</span><strong>4 คน</strong>
+																			</div>
+																			<div>
+																				<span>ประเภทเชื้อเพลิง:</span><span>&nbsp;</span><strong>แก๊ส LPG</strong>
+																			</div>
+
+																		</div>
+																	</div>
+
+																</div>
+															</div>
+															<hr>
+
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+
+									</div>
+								</div>
+								<section data-hook="progress_button">
+									<div class="row space-2 space-top-2">
+										<div class="col-sm-4">
+											<button class="btn btn-large btn-soft-dark" type="button">
+												<span>ปิด</span>
+											</button>
+										</div>
+									</div>
+								</section>
+							</div>
+						</div>
+
+						<!-- <div class="show-sm space-top-2">
+						<div class="accordion-style-checkout__section">
+						<div data-hook="mobile-total">
+						<div>
+						<div class="sidebar-text-large">
+						<div>
+						<span>ทั้งหมด</span><span>&nbsp;</span><span>฿33819</span>
+						</div>
+						</div><a href="#" data-prevent-default="true"><span>ดูการกำหนดราคาและสรุปการเดินทาง</span></a>
+						</div>
+						</div>
+						</div>
+						</div> -->
+					</div>
+				</div>
+
+			</div>
+
+		</main>
+
+		<?php
+		include 'include/footer.php';
+		?>
+	</body>
+</html>
