2327d0baa4b10689c5836b7ecd261756595b4e2d
atsdep <adthasid@hotmail.com>
2016-10-19 03:15:09 +0700

    html

---
+++ change-example.php
@@ -0,0 +1,405 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>สร้างรายได้จากการขับรถของตัวเอง กับ Rentcnd</title>
+		<meta name="description" content="">
+		<meta name="author" content="Adthasid">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<!-- css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/studyOne.css">
+		<link rel="stylesheet" href="css/studyTwo.css">
+
+		<link rel="stylesheet" href="css/adthasid.css">
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+		<?php
+		include 'config/js.php';
+		?>
+
+	</head>
+
+	<body class="with-new-header">
+
+		<?php
+		include 'include/all_header.php';
+		?>
+
+		<main id="site-content" role="main">
+			<div class="page-container page-container-responsive space-top-4 space-4" id="change-container">
+				<div>
+					<div class="panel space-4">
+						<div class="panel-header">
+							<div class="row row-table">
+								<div class="col-6 col-middle">
+									การจอง X5DH3T
+								</div>
+								<div class="col-6 col-middle">
+									<a href="/reservation/itinerary?code=X5DH3T" id="return-to-previous" class="btn pull-right"> กลับไปที่การจอง </a>
+								</div>
+							</div>
+						</div>
+
+						<div class="panel-body" id="original-details">
+							<div class="panel-padding">
+								<div class="row">
+									<div class="col-3">
+										<div class="media-photo">
+											<img src="https://a2.muscache.com/im/pictures/b163ac09-9c72-4d26-9c39-e7230a86c7f9.jpg?aki_policy=small" class="img-responsive">
+										</div>
+									</div>
+									<div class="col-6">
+										<h4 class="hosting-name"><a href="/rooms/15133897">1 Room in condo  - Supalai Park, Bang Wa BTS</a></h4>
+
+										<p class="hosting-address">
+											Soi Phet Kasem 23, กรุงเทพ, Krung Thep Maha Nakhon 10160, ไทย
+										</p>
+										<div class="user-info row">
+
+											<div class="col-sm-12">
+												<div class="media">
+													<a href="/users/show/95958096" class="media-photo media-round profile-picture pull-left"> <img src="https://a2.muscache.com/im/pictures/21da0b27-2b5b-4386-9e33-7e92e5fd06b8.jpg?aki_policy=profile_medium" width="68" height="68"> </a>
+													<div class="media-body">
+														<ul class="list-unstyled">
+															<li class="user-name">
+																Sabrina Carosi
+															</li>
+															<li>
+																<a href="mailto:sabrina-got63xkyvltlet4y@host.airbnb.com">sabrina-got63xkyvltlet4y@host.airbnb.com</a>
+															</li>
+															<li>
+																+66 989 688 114
+															</li>
+															<li>
+																<a href="/messaging/qt_for_reservation/X5DH3T">ประวัติข้อความ</a>
+															</li>
+														</ul>
+													</div>
+												</div>
+											</div>
+
+										</div>
+									</div>
+									<div class="col-3">
+										<dl>
+											<dt>
+												เช็คอิน
+											</dt>
+											<dd>
+												10-11-2016
+											</dd>
+											<dt>
+												เช็คเอาท์
+											</dt>
+											<dd>
+												11-11-2016
+											</dd>
+											<dt>
+												ผู้เข้าพัก
+											</dt>
+											<dd>
+												1
+											</dd>
+										</dl>
+									</div>
+								</div>
+								<div class="row space-top-4">
+									<div class="col-12">
+										<h4> รวมเป็นเงินทั้งสิ้น: $10 </h4>
+									</div>
+								</div>
+								<div class="row hosting-overview">
+									<div class="col-12">
+										<p>
+
+											ยอดรวมนี้รวมค่าบริการของ Airbn แล้ว
+
+											การยกเลิกและการปรับเปลี่ยนการจองนี้อยู่ภายใต้นโยบายต่อไปนี้
+
+											"<a id="cancellation-policy-link" href="#cancellation_policy" rel="closable"> ยืดหยุ่น: คืนเงินเต็มจำนวน 1 วันก่อนเดินทางมาถึง ไม่รวมค่าธรรมเนียม </a>."
+
+											แต่ละฝ่ายจัดการการจองของตัวเอง ห้ามปรับเปลี่ยนหรือยกเลิกการจองแทนอีกฝ่ายหนึ่ง
+
+											<a href="/help/article/50" target="_blank">อ่านเพิ่มเติม</a>.
+
+										</p>
+									</div>
+								</div>
+								<div id="cancellation_policy" class="row hide">
+									<div id="cancellation-policy-content" class="col-12">
+
+										<h3>ยืดหยุ่น: คืนเงินเต็มจำนวน เมื่อยกเลิกก่อนเวลาเช็คอิน 1 วัน ไม่รวมค่าธรรมเนียม</h3>
+										<ul>
+											<li>
+												ผู้เข้าพักจะได้รับค่าทำความสะอาดคืนเสมอถ้าไม่ได้เช็คอิน
+											</li>
+											<li>
+												ค่าบริการของ Airbnb ไม่สามารถเรียกคืนได้
+											</li>
+											<li>
+												รับเงินค่าที่พัก (อัตรารายคืนทั้งหมดที่คุณถูกเรียกเก็บ) คืนได้สำหรับสถานการณ์ที่ระบุด้านล่าง
+											</li>
+											<li>
+												หากมีการร้องเรียนจากฝ่ายใดก็ตาม ต้องแจ้งให้ Airbnb ทราบภายใน 24 ชั่วโมงหลังเช็คอิน
+											</li>
+											<li>
+												Airbnb จะเป็นผู้ไกล่เกลี่ยถ้าจำเป็น และจะเป็นผู้ตัดสินขั้นสุดท้ายสำหรับทุกข้อพิพาท
+											</li>
+											<li>
+												การจองจะถูกยกเลิกอย่างเป็นทางการ เมื่อผู้เข้าพักคลิกปุ่มยกเลิกในหน้ายืนยันการยกเลิกใน แดชบอร์ด &gt; การเดินทางของคุณ &gt; เปลี่ยนหรือยกเลิก
+											</li>
+											<li>
+												นโยบายการคืนเงินให้ผู้เข้าพัก เหตุสุดวิสัย หรือการยกเลิกโดย Airbnb อาจมีผลเหนือนโยบายยกเลิกการจองที่พักด้วยเหตุผลที่ได้รับอนุญาตภายใต้เงื่อนไขการให้บริการ โปรดศึกษาข้อยกเว้นเหล่านี้
+											</li>
+											<li>
+												ภาษีที่บังคับใช้จะถูกเก็บและส่งคืน
+											</li>
+										</ul>
+
+										<div class="timeline-container hide-sm">
+											<div class="row clearfix">
+												<div class="col-md-4 timeline-segment timeline-segment-refundable ">
+													<div class="timeline-point">
+														<div class="tooltip tooltip-bottom-middle dark-caret-bottom-middle timeline-point-tooltip">
+															1 วันล่วงหน้า
+														</div>
+
+														<div class="timeline-point-marker"></div>
+														<div class="timeline-point-label">
+															พ 09 พ.ย.
+															<br>
+															15:00
+														</div>
+													</div>
+												</div>
+
+												<div class="col-md-4 timeline-segment timeline-segment-partly-refundable">
+													<div id="second-point" class="timeline-point">
+														<div class="tooltip tooltip-bottom-middle dark-caret-bottom-middle timeline-point-tooltip">
+															เช็คอิน
+														</div>
+														<div class="timeline-point-marker"></div>
+														<div class="timeline-point-label">
+															พฤ 10 พ.ย.
+															<br>
+															15:00
+														</div>
+													</div>
+												</div>
+
+												<div class="col-md-4 timeline-segment timeline-segment-nonrefundable">
+													<div id="third-point" class="timeline-point">
+														<div class="tooltip tooltip-bottom-middle dark-caret-bottom-middle timeline-point-tooltip">
+															เช็คเอาท์
+														</div>
+
+														<div class="timeline-point-marker"></div>
+														<div class="timeline-point-label">
+															ศ 11 พ.ย.
+															<br>
+															11:00
+														</div>
+													</div>
+												</div>
+											</div>
+
+											<div class="timeline-fineprint">
+												วันที่และเวลาจริง
+											</div>
+										</div>
+
+										<div class="row clearfix">
+											<div class="col-md-4">
+												<p>
+													ผู้เข้าพักจะได้รับเงินคืนเต็มจำนวนหากยกเลิกการจองก่อนเวลาเช็คอิน 24 ชั่วโมง ตามเวลาท้องถิ่นของที่พัก (หรือ 15:00 น. ถ้าไม่ได้ระบุไว้) ตัวอย่างเช่น ถ้าเช็คอินวันศุกร์ จะต้องยกเลิกก่อนเวลาเช็คอินในวันพฤหัสบดีของสัปดาห์นั้น
+												</p>
+											</div>
+											<div class="col-md-4">
+												<p>
+													หากผู้เข้าพักยกเลิกภายใน 24 ชั่วโมงก่อนเวลาเช็คอิน จะไม่ได้รับเงินคืนสำหรับคืนแรก
+												</p>
+											</div>
+											<div class="col-md-4">
+												<p>
+													หากเข้าพักแล้วและออกจากที่พักก่อนวันเช็คเอาท์ จะได้รับเงินคืนเต็มจำนวนสำหรับคืนที่ไม่ได้พัก โดยนับจากวันที่ยกเลิกอย่างเป็นทางการ 24 ชั่วโมงเป็นต้นไป
+												</p>
+											</div>
+										</div>
+
+										<br>
+
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+
+					<div class="panel space-4">
+						<div class="panel-header">
+							ปรับเปลี่ยนการจองที่พัก
+						</div>
+
+						<div class="panel-body" id="alter-container">
+							<form action="/reservation_alterations" id="details-form" method="post" accept-charset="UTF-8">
+								<input name="authenticity_token" type="hidden" value="V4$.airbnb.com$TLPu3xmYlek$2HIYQhRJkc-rZlAGQBPr_B7ni5Ci9txVQlKXSFtbeUA=">
+								<input type="hidden" name="code" value="X5DH3T">
+								<div class="row overview-inputs">
+									<div class="col-3">
+										<label for="pricing[hosting_id]">ที่พัก</label>
+										<div class="select select-block">
+											<select name="pricing[hosting_id]" id="property">
+
+												<option value="15133897" selected="selected" data-original="original">1 Room in condo  - Supalai Park, Bang Wa BTS</option>
+
+											</select>
+										</div>
+										<label for="pricing_guests">ผู้เข้าพัก</label>
+										<div class="select">
+											<select class="num-guests" id="pricing_guests" name="pricing[guests]">
+
+												<option selected="selected" data-original="original">1 </option>
+
+												<option>2 </option>
+
+												<option>3 </option>
+
+												<option>4 </option>
+
+												<option>5 </option>
+
+												<option>6 </option>
+
+												<option>7 </option>
+
+												<option>8 </option>
+
+												<option>9 </option>
+
+												<option>10 </option>
+
+												<option>11 </option>
+
+												<option>12 </option>
+
+												<option>13 </option>
+
+												<option>14 </option>
+
+												<option>15 </option>
+
+												<option>16+ </option>
+
+											</select>
+										</div>
+									</div>
+									<div class="col-3">
+										<label for="pricing_start_date">เช็คอิน</label>
+										<input type="text" class="datepicker checkin ui-datepicker-target" id="pricing_start_date" name="pricing[start_date]" value="10-11-2016">
+										<label for="pricing_end_date">เช็คเอาท์</label>
+										<input type="text" class="datepicker checkout ui-datepicker-target" id="pricing_end_date" name="pricing[end_date]" value="11-11-2016">
+									</div>
+									<div class="col-6">
+										<div id="new-subtotal-container">
+
+											<table class="table">
+												<tbody>
+													<tr>
+														<th class="new-subtotal-string">ยอดรวมย่อย</th>
+														<td id="new-subtotal">$9</td>
+													</tr>
+													<tr>
+														<th>ค่าบริการ:</th>
+														<td id="service-fee">$1</td>
+													</tr>
+													<tr id="taxes-row" class="hide">
+														<th> ภาษีการครอบครอง <i class="icon icon-question" data-behavior="tooltip" id="tax_descriptions_tooltip"> </i></th>
+														<td id="taxes">$0</td>
+													</tr>
+												</tbody>
+												<tfoot>
+													<tr>
+														<th>ยอดรวมใหม่</th>
+														<td><span id="new-total">$10</span></td>
+													</tr>
+													<tr class="hide" id="price-difference-container">
+														<th>ส่วนต่าง:</th>
+														<td><span id="price-difference"></span></td>
+													</tr>
+												</tfoot>
+											</table>
+
+											<div class="row alteration-check">
+												<div class="col-12">
+													<span class="available icon-lima"></span>
+													<span class="unavailable icon-rausch"></span>
+												</div>
+											</div>
+										</div>
+									</div>
+
+								</div>
+							</form>
+							<div class="row space-top-4">
+								<div class="col-12">
+									<p id="new-cancellation-policy" style="display: none;">
+										หากมีการยอมรับการเปลี่ยนแปลง การจองใหม่จะอยู่ภายใต้เงื่อนไขของนโยบายนี้ <a href="#" target="_blank"></a>
+									</p>
+									<p>
+
+										การปรับเปลี่ยนนี้ขึ้นอยู่กับสถานะว่างของที่พักและต้องได้รับการอนุมัติจากผู้เข้าพักและเจ้าของที่พัก หากมีการยอมรับการเปลี่ยนแปลง เราจะเรียกเก็บเงินจากผู้เข้าพัก หรือคืนเงินส่วนต่างระหว่างมูลค่าการจองเก่าและใหม่ให้ผู้เข้าพัก ระบบจะปรับปรุงค่าบริการของ Airbnb ให้สอดคล้องกับราคาที่เปลี่ยนไป
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="panel-footer" id="alter-buttons">
+							<a href="#" class="btn" id="discard-alterations"> ยกเลิกการปรับเปลี่ยน </a>
+							<a class="btn btn-primary" id="alter-submit-button" href="#" disabled="disabled" data-prevent-default=""> ส่งการปรับเปลี่ยน </a>
+						</div>
+
+					</div>
+
+					<div class="modal" role="dialog" data-trigger="#alter-submit-button" aria-hidden="true">
+						<div class="modal-table">
+							<div class="modal-cell">
+								<div class="modal-content">
+									<div class="panel-header">
+										เปลี่ยนการจอง
+									</div>
+									<div class="panel-body">
+										<p>
+											คุณแน่ใจหรือว่าคุณต้องการเสนอการเปลี่ยนแปลงเหล่านี้
+										</p>
+									</div>
+									<div class="panel-footer">
+										<a class="btn" href="#" data-behavior="modal-close">ไม่</a>
+										<a href="#" id="alteration-submit" class="btn btn-primary"> ใช่ เสนอการปรับเปลี่ยนเหล่านี้ </a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</main>
+
+		<?php
+		include 'include/footer.php';
+		?>
+	</body>
+</html>
