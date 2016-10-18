3becd4eef8a36a5f8370f27aad7a38397a58b9bb
atsdep <adthasid@hotmail.com>
2016-10-19 03:16:04 +0700

    html

---
+++ alterations-example.php
@@ -0,0 +1,141 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>สร้างรายได้จากการขับรถของตัวเอง กับ Rentcnd</title>
+		<meta name="description" content="">
+		<meta name="author" content="Adthasid">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<!-- css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/studyOne.css">
+		<link rel="stylesheet" href="css/studyTwo.css">
+
+		<link rel="stylesheet" href="css/adthasid.css">
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+		<?php
+		include 'config/js.php';
+		?>
+	</head>
+
+	<body class="with-new-header">
+
+		<?php
+		include 'include/all_header.php';
+		?>
+
+		<main id="site-content" role="main">
+			<div data-hypernova-key="alterationsbundlejs">
+				<div>
+					<div class="page-container-responsive">
+
+						<div class="row space-top-sm-2 space-top-8 change-or-cancel">
+							<div class="col-offset-sm-1 col-offset-md-0 col-offset-lg-1 col-sm-12 col-md-5 col-lg-4 pull-right">
+								<div class="reservation-card">
+									<div class="top-image col-sm-4 col-md-12 space-2 space-sm-0" style="background-image:url(&quot;https://a2.muscache.com/im/pictures/b163ac09-9c72-4d26-9c39-e7230a86c7f9.jpg?aki_policy=medium&quot;);"></div>
+									<div class="show-sm col-sm-8 space-top-2">
+										<div>
+											<h4 class="space-sm-0 space-top-2"><a href="/rooms/15133897" target="_blank">1 Room in condo  - Supalai Park, Bang Wa BTS</a></h4>
+											<div class="text-muted">
+												<span>พฤศจิกา 10</span><span> –&nbsp;</span><span>พฤศจิกา 11</span>
+											</div>
+										</div>
+									</div>
+									<div class="panel-body hide-sm">
+										<strong><span>พฤศจิกา 10</span><span> –&nbsp;</span><span>พฤศจิกา 11</span></strong><h4 class="space-sm-0 space-top-2"><a href="/rooms/15133897" target="_blank">1 Room in condo  - Supalai Park, Bang Wa BTS</a></h4>
+										<div class="text-muted">
+											<span><span>ให้เช่าโดย</span><span>&nbsp;</span><a href="/users/show/95958096" target="_blank">Sabrina</a></span>
+										</div>
+										<div class="text-muted">
+											<span>1 คืน</span><span> – </span><span>ผู้เข้าพัก 1 คน</span>
+										</div>
+										<hr>
+										<div class="hide-sm">
+											<div class="pull-right">
+												<a href="/home/cancellation_policies#flexible" target="_blank">ยืดหยุ่น</a>
+											</div><span>นโยบายยกเลิกการจองที่พัก</span>
+											<hr>
+										</div>
+										<div class="space-1 hide-sm">
+											<strong>รายละเอียดการชำระเงิน</strong>
+										</div>
+										<div class="hide-sm">
+											<div class="pull-right">
+												<span>$9</span>
+											</div><span>$9 x 1 คืน</span>
+										</div>
+										<div>
+											<div class="pull-right">
+												<span>$1</span>
+											</div><span>ค่าบริการ Airbnb </span>
+										</div>
+										<hr>
+										<div>
+											<div class="final-section">
+												<div>
+													<div class="pull-right">
+														<span>$10</span>
+													</div><span>ที่จ่ายทั้งหมด</span>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-12 space-top-sm-4 space-top-md-0 col-md-7">
+								<div class="space-md-4">
+									<h2><small>คุณต้องการทำอะไร</small></h2>
+								</div><a href="/reservation/change?code=X5DH3T&amp;visited=1" class="link-reset">
+								<div class="panel space-2">
+									<div class="panel-body panel-body-change-or-cancel row">
+										<div class="col-sm-10 no-left-padding-sm col-lg-push-1">
+											<h2 class="space-1"><small>เปลี่ยนการจองที่พัก</small></h2>
+											<div class="text-muted hide-sm">
+												เปลี่ยนวันที่เดินทาง ที่พักหรือจำนวนผู้เข้าพัก
+											</div>
+										</div>
+										<div class="centered-padding icon-div col-sm-2 space-top-md-3 space-top-lg-2 col-lg-pull-10 col-lg-2">
+											<i class="icon icon-rausch icon-size-2 icon-entire-place"></i>
+										</div>
+									</div>
+								</div></a><a href="#" data-prevent-default="true" class="link-reset">
+								<div class="panel space-2">
+									<div class="panel-body panel-body-change-or-cancel row">
+										<div class="col-sm-10 no-left-padding-sm col-lg-push-1">
+											<h2 class="space-1"><small>ยกเลิกการจองที่พัก</small></h2>
+											<div class="text-muted hide-sm">
+												การจองที่พักนี้มีนโยบายยกเลิกการจองที่พักยืดหยุ่น
+											</div>
+										</div>
+										<div class="centered-padding col-sm-2 icon-div space-top-md-3 space-top-lg-2 col-lg-pull-10 col-lg-2">
+											<i class="icon icon-rausch icon-size-2 icon-ban-circle"></i>
+										</div>
+									</div>
+								</div></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</main>
+
+		<?php
+		include 'include/footer.php';
+		?>
+	</body>
+</html>
