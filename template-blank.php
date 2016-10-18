c8fc0a32904d9323e5ed724947db6325daa2d8f6
atsdep <adthasid@hotmail.com>
2016-10-19 05:06:49 +0700

    template-blank

--- template-blank.php
+++ template-blank.php
@@ -41,69 +41,12 @@ session_start();
 		?>
 
 		<main id="site-content" role="main">
-
+			
 			
 
 		</main>
 
-		<div id="signup-modal" class="modal fade" role="dialog" aria-describedby="signup-modal-content"  tabindex="-1">
-			<div class="">
-				<div class="">
-					<div class="modal-content signup signup-login-form__container" id="signup-modal-content">
-
-						<div class="modal-header panel-header">
-							ลงทะเบียน
-							<a href="#" class="modal-close" data-dismiss="modal" data-behavior="modal-close"></a>
-						</div>
-
-						<div class="alert alert-with-icon alert-error alert-header panel-header hidden-element notice" id="notice">
-							<i class="icon alert-icon icon-alert-alt"></i>
-
-						</div>
-
-						<div class="panel-padding panel-body signup-login-form__extra-panel-body first">
-
-							<div class="social-buttons">
-
-								<a href="/oauth_connect?from=facebook_signup&amp;redirect_url=%2Frooms%2Fnew%3Ffrom_sh%3D1&amp;service=facebook" class="btn icon-btn create-using-facebook btn-block signup-login-form__btn-xl space-1 btn-large btn-facebook js-facebook-auth"> <span class="icon-container"> <i class="icon icon-facebook"></i> </span> <span class="text-container"> ลงทะเบียนโดยใช้ Facebook </span> </a>
-
-							</div>
-
-							<div class="text-center social-links hide">
-								สมัครด้วย <a href="#" class="js-facebook-auth">Facebook</a>
-							</div>
-
-							<div class="signup-or-separator">
-								<span class="h6 signup-or-separator--text">หรือ</span>
-								<hr>
-							</div>
-
-							<!--[if (gt IE 8)|!(IE)]><!-->
-							<div class="text-center">
-								<a href="/signup_login?sm=2" class="create-using-email btn-block signup-login-form__btn-xl space-2 btn btn-soft btn-block signup-login-form__btn-xl btn-large large icon-btn" id="create_using_email_button"> <i class="icon icon-envelope"></i> ลงทะเบียนโดยใช้อีเมล </a>
-							</div>
-							<!--<![endif]-->
-							<!--[if (lte IE 8) & IE]>
-							<div class="text-center">
-							<a href="/signup_login?sm=2" class="create-using-email btn-block signup-login-form__btn-xl space-2 btn btn-primary btn-block signup-login-form__btn-xl btn-large large icon-btn" id="create_using_email_button">
-							<i class="icon icon-envelope"></i>
-							ลงทะเบียนโดยใช้อีเมล
-							</a>  </div>
-							<![endif]-->
-
-							
-						</div>
-
-						<div class="panel-body js-login-footer signup-login-form__extra-panel-body">
-							<div class="clearfix">
-								<span class="signup-login-form__switch-copy"> มีบัญชีผู้ใช้ rentcnd อยู่แล้วใช่ไหม </span>
-								<a href="/login?" class="modal-link link-to-login-in-signup btn btn-default signup-login-form__btn-soft-border signup-login-form__btn-font-normal signup-login-form__switch-button" data-modal-href="/login_modal?" data-modal-type="login"> เข้าสู่ระบบ </a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		
 
 		<?php
 		include 'include/footer.php';
